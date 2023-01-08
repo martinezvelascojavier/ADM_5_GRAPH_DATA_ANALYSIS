bcb7f3e20de2048aee858cc524d1a8a215d399bf
Iadiserniag <115364605+Iadiserniag@users.noreply.github.com>
2023-01-08 17:54:42 +0100

    Add files via upload

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # We have ran these commands on clean datasets after doing the required preprocessing and removing the commas (",") within hero's names and comic's names,
-#as we find out that the csv file was reading the content wrongly on these cases. These clean datasets are hero-network-preprocessed.csv and edges_preprocessed.csv
+# as we find out that the csv file was reading the content wrongly on these cases. These clean datasets are hero-network-preprocessed.csv and edges_preprocessed.csv
 
 #1. Most popular pair of heroes
 
 echo 'The most popular pair of hereos is:'
 cut -d, -f1,2 hero-network-preprocessed.csv | sort | uniq -c | sort -nr | head -n 1
 
-#ANSWER: The most popular pair of heros is:
-#1267 PATRIOT/JEFF MACE,MISS AMERICA/MADELIN
+# ANSWER: The most popular pair of heros is:
+# 1267 PATRIOT/JEFF MACE, MISS AMERICA/MADELIN
 
 #2. Number of comics per hero.
 
@@ -25,9 +25,10 @@ cut -d, -f1 edges_preprocessed.csv | sort | uniq -c | sort -nr
 
 #3. Average comics per hero:
 
+echo " "
 echo 'The average number of heroes per comic is:'
 cut -d, -f2 edges_preprocessed.csv | sort | uniq -c | sort -nr > comics_per_hero.csv
 awk '{ total += $1 } END { print total/NR }' comics_per_hero.csv
 
 # ANSWER: The average number of heroes per comic is:
-#7.59663
+# 7.59663
